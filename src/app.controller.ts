038860792fa46b421513946501386f486d8ebb8d
FazekasMarton <fazekas.marton16@gmail.com>
2024-09-20 16:46:42 +0200

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -11,5 +11,35 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get("/quotes")
+  @Render('quotes')
+  getQuotes() {
+    return this.appService.getQuotes()
+  }
+
+  @Get("/randomQuote")
+  @Render('randomQuote')
+  getRandomQuote() {
+    return this.appService.getRandomQuote()
+  }
+
+  @Get("/topAuthors")
+  @Render('topAuthors')
+  getTopAuthors() {
+    return this.appService.getTopAuthors()
+  }
+
+  @Get('quotes/:id')
+  @Render('randomQuote')
+  oneQuote(@Param('id') id: string) {
+    return this.appService.getQuotesById(id)
+  }
+
+  @Get('deleteQuote/:id')
+  deleteQuote(@Param('id') id: string) {
+    let deleteQuote = this.appService.deleteQuote(id)
+    return (deleteQuote ? "Sikeres törlés" : "Ismeretlen idézet")
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { quotes } from './quotes';
+import { randomInt } from 'crypto';
+
+let new_quotes = JSON.parse(JSON.stringify(quotes))
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  getQuotes() {
+    return new_quotes
+  }
+
+  getRandomQuote() {
+    let quote = new_quotes.quotes[randomInt(new_quotes.quotes.length - 1)]
+    return quote
+  }
+
+  getTopAuthors() {
+    let authorCount: { [key: string]: number } = {};
+    new_quotes.quotes.forEach(quote => {
+      if (authorCount[quote.author]) {
+        authorCount[quote.author]++;
+      } else {
+        authorCount[quote.author] = 1;
+      }
+    });
+
+    let sortedAuthors = Object.entries(authorCount).sort((a, b) => {
+      return (b[1] as number) - (a[1] as number);
+    });
+
+    return {topA: sortedAuthors}
+  }
+
+  getQuotesById(id: string){
+    let selected_quote = {}
+    quotes.quotes.some(quote => {
+      if(quote.id == Number(id)){
+        selected_quote = quote
+        return true
+      }
+      return false
+    });
+    return selected_quote
+  }
+
+  deleteQuote(id: string){
+    let succeed = false
+    quotes.quotes.some((quote, i) => {
+      if(quote.id == Number(id)){
+        quotes.quotes.splice(i, 1)
+        succeed = true
+        return true
+      }
+      return false
+    });
+    return succeed
   }
 }
